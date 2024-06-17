b91d355d3e5dd7c970ba2e37bc117a0a118aa964
Naseef <naseefhassanmgmail.com>
2024-06-17 20:10:23 +0530

    topbar added

--- src/Components/Bar/AppBar.jsx
+++ src/Components/Bar/AppBar.jsx
@@ -3,7 +3,7 @@ import add from '../../assets/Images/add-user.png'
 import bell from '../../assets/Images/bell.png'
 function AppBar() {
   return (
-    <div className="bg-[#6fccdd] flex justify-between p-7 border-4 border-white ">
+    <div className="bg-[#6fccdd] flex justify-between p-9 border-4 border-white rounded-xl ">
        <img className='w-6 cursor-pointer' src={account} alt="" />
        <img className='w-6 cursor-pointer' src={add} alt="" />
        <img className='w-6 cursor-pointer' src={bell} alt="" />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,12 +5,12 @@ import TopImage from '../assets/Images/Light_Mode_TopImage.svg'
 import BackgroundImg from '../assets/Images/Light_Mode_Background.svg'
 function Home() {
   return (
-    <div style={{backgroundImage:`url(${BackgroundImg})`}} className=' w-screen h-screen bg-no-repeat relative flex bg-cover mx-auto  '>
-        {/* <div className='flex justify-between'>
-           <div className='w-1/2'> <Header/></div>
-           <div className='w-5/12'>  <AppBar/></div>
-        </div>
-        <div className='flex absolute bottom-0'>
+    <div style={{backgroundImage:`url(${BackgroundImg})`}} className=' w-screen h-screen bg-no-repeat relative flex bg-cover mx-auto '>
+        {/* <div className='flex  justify-between'> */}
+           <div className='w-7/12'> <Header/></div>
+           <div className='w-[515px] absolute right-0 '>  <AppBar/></div>
+        {/* </div> */}
+        {/* <div className='flex absolute bottom-0'>
             <div className='w-10 h-full'><SideBar/></div>
         </div>
         <img className='w-5/12 absolute right-0 z-10 top-[85px]' src={TopImage} alt="" /> */}
